ffb487a440378bcdd9c184dad162749b84cc4fb1
mmmmmagina <jaice_229@163.com>
2014-06-25 22:08:39 +0800

    fix script

--- shell/prod_generateZip.sh
+++ shell/prod_generateZip.sh
@@ -1,7 +1,7 @@
 cd /var/coinport/code/admin/
-releaseBranch=`git branch -a | grep $1`
+releaseBranch=`git branch | grep $1`
 if [ -z "$releaseBranch" ];then
-  git checkout -b $1 remotes/origin/$1
+  git checkout -b $1 origin/$1
 else
   git checkout $1
 fi
