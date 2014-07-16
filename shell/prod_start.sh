ce402922dde412fb5942f5c3395330e72112fa1c
mmmmmagina <jaice_229@163.com>
2014-07-16 22:49:52 +0800

    update deploy script

--- shell/prod_start.sh
+++ shell/prod_start.sh
@@ -1,2 +1,3 @@
 cd /var/coinport/admin/coinport-admin-*/bin/
-nohup ./coinport-admin -Dhttp.port=9090 -Dhttp.address=172.31.1.67 -Dakka.config=akka-prod.conf &
+#nohup ./coinport-admin -Dhttp.port=9090 -Dhttp.address=172.31.1.67 -Dakka.config=akka-prod.conf &
+nohup ./coinport-admin -Dhttp.port=9090 -Dakka.config=akka-prod.conf &
